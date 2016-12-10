2c07f8b743064c49729d32c754f79f9d137a60ad
Wlodek Bzyl <matwb@univ.gda.pl>
2016-12-10 12:18:00 +0100

    update fact.c with printf with locales

--- 300-Rekurencja/fact.c
+++ 300-Rekurencja/fact.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <locale.h>
 
-int Fact(int n) {
+long unsigned int Fact(int n) {
   if (n == 0)
     return 1;
   else
-    return n * Fact(n-1);
+    return n * Fact(n - 1);
 }
 
 int main() {
-  int i;
-  for (i=0; i < 11; i++)
-    printf("\t%8d\n", Fact(i));
+  setlocale(LC_NUMERIC, ""); // use the native environment
+
+  for (int i = 0; i <= 20; i++)
+    printf("%4d %'28ld\n", i, Fact(i));
 }
