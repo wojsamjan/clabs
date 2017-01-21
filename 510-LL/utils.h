c5ccaa07cdfcce71f828a26188c777d006c786b1
Wlodek Bzyl <matwb@univ.gda.pl>
2017-01-21 12:47:25 +0100

    update utils.h

--- 510-LL/utils.h
+++ 510-LL/utils.h
@@ -6,17 +6,17 @@ struct node {
   struct node* next;
 };
 
-/* zwr�� liczb� w�z��w na li�cie. */
+/* zwróć liczbę  węzłów na liście */
 int Length(struct node* head);
 
-/* dodaj nowy w�ze�, do listy *headRef
-   i umie�� w w��le w polu .data liczb� newData. */
+/* dodaj nowy węzeł do listy *headRef
+   i umieść w węźle w polu .data liczbę newData */
 void Push(struct node** headRef, int newData);
 
-/* zbuduj list� {1, 2, 3} i zwr�� wska�nik do niej. */
+/* zbuduj listę {1, 2, 3} i zwróć wskaźnik do niej */
 struct node* BuildOneTwoThree(void);
 
-/* wypisz wszystkie elementy listy head. */
+/* wypisz wszystkie elementy listy head */
 void ShowLinkedList(struct node* head);
 
 #endif
